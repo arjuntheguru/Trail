2af10c063990968a69b35b1fedf3c0e7aaf70734
Arjun Subedi <arjunsubedi@live.com>
2021-06-06 21:54:45 +0545

    Added SiteId and IsActive properties in UserDTO

--- Trail.Application/Common/Models/DTO/UserDTO.cs
+++ Trail.Application/Common/Models/DTO/UserDTO.cs
@@ -14,7 +14,9 @@ namespace Trail.Application.Common.Models.DTO
         public string Email { get; set; }
         public string UserName { get; set; }
         public string CompanyId { get; set; }
+        public bool IsActive { get; set; }
         public string Role { get; set; }
         public string Token { get; set; }
+        public IEnumerable<string> SiteId { get; set; }
     }
 }
