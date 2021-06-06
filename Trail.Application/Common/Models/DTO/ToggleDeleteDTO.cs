fd4e222c097600a99d5d140ff48fc50b45e06984
Arjun Subedi <arjunsubedi@live.com>
2021-06-06 21:54:07 +0545

    Added ToogleDeleteDTO

---
+++ Trail.Application/Common/Models/DTO/ToggleDeleteDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trail.Application.Common.Models.DTO
+{
+    public class ToggleDeleteDTO
+    {
+        public string Id { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
